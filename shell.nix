42e013b45054b834a6862a4b22eea31e34d3e056
Eric Hall <ewhall18@gmail.com>
2025-11-12 01:50:14 +0000

    fixes to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,17 +1,20 @@
 #!/user/bin/env nix-shell
-{ pkgs ? import<nixpkgs> { } }:
+{ pkgs ? import<nixpkgs> { } }: 
 (
   pkgs.buildFHSEnv {
     name = "ece573-project-FHS";
     targetPkgs = pkgs: (with pkgs; [
-      gcc glibc boost scons pre-commit protobuf protobufc gperftools hdf5 gnum4 capstone libpng libelf wget cmake doxygen clang-tools
+      pkg-config
+      gcc glibc boost scons pre-commit protobuf_21 protobufc gperftools hdf5 hdf5-cpp gnum4 capstone libpng libelf wget cmake doxygen clang-tools
       curl libmpc mpfr gmp texinfo gperf patchutils bc zlib expat libslirp flex bison
       python313 ] ++ (with python313Packages; [
+        virtualenv
+        pip
+        pyyaml
         pydot
         mypy
         tkinter
         pkgconfig
-        pip
 	protobuf
       ])
     );
